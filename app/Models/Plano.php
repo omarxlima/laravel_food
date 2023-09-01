7e54905c55c2fc91c86dc26e340a3572b380418b
marxlima <marxmaciel@gmail.com>
2023-09-01 00:23:49 -0300

    instalacao do adminlte3

---
+++ app/Models/Plano.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plano extends Model
+{
+    use HasFactory;
+
+    protected $table = 'planos';
+
+    protected $fillable = [
+        'nome',
+        'url',
+        'preco',
+        'descricao'
+    ];
+}
